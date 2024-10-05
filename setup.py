95616b18e37e6bca12d9f9f31c2873b5a37bfebc
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-10-05 21:26:18 +0800

    fix: License name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -6,17 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="gewechat-client",
     version="0.1.0",
-    author="Gewechat",
-    author_email="contact@gewechat.com",
+    author="hanfangyuan",
+    author_email="i@hanfangyuan.cn",
     description="A package for interacting with the Gewechat API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/gewechat-python-github",
-    license='MIT',
-    packages=find_packages(exclude=["reference-code"]),
+    url="https://github.com/hanfangyuan4396/gewechat-python",
+    license='Apache-2.0',
+    packages=['gewechat_client'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
